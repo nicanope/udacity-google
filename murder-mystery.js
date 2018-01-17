feaf5f3919b2009abe90d92c6e2ed2e0c9e40654
Nicanope <nicole.motles@gmail.com>
2018-01-17 13:09:34 +0100

    JS muder mystery file
    code for the murder mystery challenge / Lesson 12.8

---
+++ murder-mystery.js
@@ -0,0 +1,33 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room==="ballroom" && suspect==="Mr. Kalehoff") {
+   solved=true;
+   weapon="poison";
+} else if (room==="gallery" && suspect==="Ms. Van Cleve") {
+    solved=true;
+    weapon="trophy";
+} else if (room==="billiards room" && suspect==="Mrs. Sparr") {
+    solved=true;
+    weapon="pool stick";
+} else if (room==="dining room" && suspect==="Mr. Parkes") {
+    solved=true;
+    weapon="knife";
+} else {
+    solved=false;
+}
+
+if (solved) {
+	console.log(suspect+" did it in the "+room+" with the "+weapon+"!");
+} else {
+    console.log("Mystery not solved!");
+}
+
